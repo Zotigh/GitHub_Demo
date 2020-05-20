d9ca49e5e4c5ea0c13b06d24eca0e44475b0b7b2
Lance Zotigh <lancezotighlz@gmail.com>
2020-05-20 16:14:41 -0600

    Added a line to display my name.

--- GitHUb_Demo/Program.cs
+++ GitHUb_Demo/Program.cs
@@ -7,6 +7,7 @@ namespace GitHUb_Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Lance Zotigh");
         }
     }
 }
